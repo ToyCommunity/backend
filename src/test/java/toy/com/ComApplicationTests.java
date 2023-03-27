0b299aa88d4bb6204f07390d18e2f22576d8a031
박종빈 <runnz121@gmail.com>
2023-03-27 23:17:14 +0900

    first commit

---
+++ src/test/java/toy/com/ComApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

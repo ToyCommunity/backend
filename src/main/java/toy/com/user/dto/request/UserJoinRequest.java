a8c2c268f3b756b35dbcdcbc978d95025475c786
youngreal <59333182+youngreal@users.noreply.github.com>
2023-04-10 12:56:07 +0900

    Feat: 회원가입 정규패턴식 추가

--- src/main/java/toy/com/user/dto/request/UserJoinRequest.java
+++ src/main/java/toy/com/user/dto/request/UserJoinRequest.java
@@ -2,6 +2,7 @@ package toy.com.user.dto.request;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -12,17 +13,17 @@ import toy.com.user.domain.User;
 @Builder
 public record UserJoinRequest(
 	@NotBlank(message = "이메일을 입력해 주세요")
-	@Email @Size(min = 8, max = 50)
+	@Email @Size(min = 8, max = 50) @Pattern(regexp = "^[\\\\w!#$%&'*+/=?`{|}~^-]+(?:\\\\.[\\\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\\\.)+[a-zA-Z]{2,6}$\"")
 	@Schema(description = "유저 이메일")
 	String email,
 
 	@NotBlank(message = "password를 입력해 주세요")
-	@Size(min = 8, max = 20)
+	@Size(min = 8, max = 20) @Pattern(regexp = "^[a-zA-Z0-9가-힣]+$")
 	@Schema(description = "유저 password")
 	String password,
 
 	@NotBlank(message = "닉네임을 입력해주세요")
-	@Size(min = 2, max = 10)
+	@Size(min = 2, max = 10) @Pattern(regexp = "^[a-zA-Z0-9가-힣]+$")
 	@Schema(description = "유저 nickname")
 	String nickname
 ) {

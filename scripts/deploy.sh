b8055d635296040e50c9b8cabafc1d58b622213d
박종빈 <runnz121@gmail.com>
2023-05-03 21:22:48 +0900

    fix: script 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
 REPOSITORY=/home/ec2-user/toy_com
-JAR_PATH="$REPOSITORY/build/libs/*.jar"
+JAR_PATH=$REPOSITORY/build/libs/*.jar
 BUILD_JAR=$(ls $JAR_PATH)
-JAR_NAME=${basename $BUILD_JAR}
-
+JAR_NAME=$(basename $BUILD_JAR)
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]

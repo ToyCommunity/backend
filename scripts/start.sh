ea7c0d1039764e46491ed1e39c903249b73da495
박종빈 <runnz121@gmail.com>
2023-05-07 15:23:40 +0900

    feature: 레디스 서버 없이 실행 로직 추가

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,4 +3,4 @@
 REPOSITORY="/home/ec2-user/toy_com"
 sleep 5
 echo "> $JAR_PATH start"
-sudo nohup java -jar /home/ec2-user/toy_com/build/libs/com-0.0.1-SNAPSHOT.jar > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar /home/ec2-user/toy_com/build/libs/com-0.0.1-SNAPSHOT.jar > $REPOSITORY/nohup.out 2>&1 &
